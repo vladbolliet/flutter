6af2559d6ac3aeea0b92d0c87b58ba8b11cf6ab2
vlad <bollietvladuts@gmail.com>
2025-03-11 23:55:12 +0100

    added container when clicking start new button

--- self_improvement_app/features/task_timer/task_timer_page.dart
+++ self_improvement_app/features/task_timer/task_timer_page.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
-class TaskTimerPage extends StatelessWidget {
+class TaskTimerPage extends StatefulWidget {
+  @override
+  TaskTimerPageState createState() => TaskTimerPageState();
+}
+
+class TaskTimerPageState extends State<TaskTimerPage> {
+
+  bool _StartNewTaskIsExpanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,50 +16,82 @@ class TaskTimerPage extends StatelessWidget {
         title: Text('Task Timer'),
         centerTitle: true,
         ),
-      body: Column(
+      body: Stack(
         children: [
-          // start new task button
-          Padding(
-            padding: EdgeInsets.only(
-              top: MediaQuery.of(context).size.height * 0.3,
-            ),
-            child: Align(
-              alignment: Alignment.center,
-              child: Container(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    textStyle: TextStyle(fontSize: 24),
+          Column(
+            children: [
+              // start new task button
+              if(!_StartNewTaskIsExpanded) ... [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.3,
                   ),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: TextStyle(fontSize: 24),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text("+ Start new task"),
+                        ),
+                        onPressed: () {
+                          print("pressed new task button\n");
+                          setState(() {
+                            _StartNewTaskIsExpanded = true;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.03,),
+                  child: TextButton(
+                    style: TextButton.styleFrom(textStyle: TextStyle(fontSize: 24)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.edit, color: Colors.blue),
+                          SizedBox(width: 8),
+                          Text("Manual input"),
+                        ]
+                      ),
+                    ),
+                    onPressed: () => print("pressed manual input button\n"),
+                  )
+                )
+              ]
+              else ... [
+                AnimatedPositioned(
+                  duration: Duration(milliseconds: 500),
                   child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text("+ Start new task"),
+                    padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.2,
+                    ),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 36, 36, 36), // Background color
+                          borderRadius: BorderRadius.circular(16), // Rounded corners
+                        ),
+                        height: MediaQuery.of(context).size.height * 0.55,
+                        width: MediaQuery.of(context).size.width * 0.85,
+                      ),
+                    ),
                   ),
-                  onPressed: () => print("pressed new task button\n"),
                 ),
-              ),
-            ),
-          ),
-          // manual input button
-          Padding(
-            padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.03,),
-            child: TextButton(
-              style: TextButton.styleFrom(textStyle: TextStyle(fontSize: 24)),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.edit, color: Colors.blue),
-                    SizedBox(width: 8),
-                    Text("Manual input"),
-                  ]
-                ),
-              ),
-              onPressed: () => print("pressed manual input button\n"),
-            )
-          )
-      ],)
+              ],
+              // end of the if statement
+          ],),
+        ],
+      )
     );
   }
 }
